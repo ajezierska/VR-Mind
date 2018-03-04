e6beb28814ba680192e61693d5b9196bdb09e9e1
ajezierska <a.b.jezierska@gmail.com>
2018-03-04 13:32:26 +0100

    smooth scroll

---
+++ js/app.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+         $("nav a").on('click', function(event) {
+             var hash = this.hash;
+             $('html, body').animate({
+               scrollTop: $(hash).offset().top
+             }, 800, function(){
+               window.location.hash = hash;
+             });
+           }
+       )});
